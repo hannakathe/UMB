8c40c8b9375962d44681953b2ccb0353446ee116
Hannah K Abril <abrilhanna6@gmail.com>
2025-02-26 18:10:44 -0500

    Update FlotaUI.java

--- Tercer_Semestre/Estructura_Datos/Lab_2/VentasBuses/src/ui/FlotaUI.java
+++ Tercer_Semestre/Estructura_Datos/Lab_2/VentasBuses/src/ui/FlotaUI.java
@@ -6,7 +6,6 @@ import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-//import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.List;
 
